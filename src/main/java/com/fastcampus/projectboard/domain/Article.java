39116a830cf590ae6eff4477f457d2c4abff772b
sungbae85 <hsbjyo@naver.com>
2023-02-01 22:20:14 +0900

    #13-엔터티 2개 설정
    Article,  ArticleComment 설정

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,17 +1,83 @@
 package com.fastcampus.projectboard.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;   // id
-    private String title;   // 게시글제목
-    private String content;   // 내용
-    private String hashtag;   // 해시태그
 
-    private LocalDateTime createdAt;  // 생성일자
-    private String createdBy;   // 생성자
-    private LocalDateTime modifiedAt;   // 수정일자
-    private String modifiedBy;   // 수정자
+
+    @Setter @Column(nullable = false, length = 255) private String title;   // 게시글제목
+    @Setter @Column(nullable = false, length = 10000) private String content;   // 내용
+
+    @Setter @Column(length = 255) private String hashtag;   // 해시태그
+
+    //loop를 끊기 위해서  @ToString.Exclude를 선언한다
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+//    @OneToMany(mappedBy = "article")
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate      @Column(nullable = false)               private LocalDateTime createdAt;  // 생성일자
+    @CreatedBy        @Column(nullable = false, length = 100) private String        createdBy;   // 생성자
+    @LastModifiedDate @Column(nullable = false)               private LocalDateTime modifiedAt;   // 수정일자
+    @LastModifiedBy   @Column(nullable = false, length = 100) private String        modifiedBy;   // 수정자
+
+    protected Article() {
+    }
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of (String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+//        if (!(o instanceof Article article1)) return false;
+
+        return id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
 
 }
