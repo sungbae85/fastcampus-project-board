490bc023bbb81fc2abd62ee3f40b96877041086b
sungbae85 <hsbjyo@naver.com>
2023-02-04 08:49:43 +0900

    #4 - rest-hal-explorer
    data-rest, rest-hal-explorer 브라우즈를 통하여 조회하는 방법을 학습
    액세스를 위한 yaml파일 정의 - data.rest.base-path: /api
    localhost:8080/api

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -2,8 +2,10 @@ package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 //@Repository 를 붙이면 안된다
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
